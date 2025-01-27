289114244c73e28fb014554137aec6e857998b5f
egilkh <egilkh@gmail.com>
2025-01-27 20:48:00 +0100

    wip

--- cdk/bin/cloud-gaming-on-ec2.ts
+++ cdk/bin/cloud-gaming-on-ec2.ts
@@ -1,25 +1,30 @@
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import 'source-map-support/register';
-import { G4DNStack } from '../lib/g4dn';
 import { G4ADStack } from '../lib/g4ad';
+import { G4DNStack } from '../lib/g4dn';
 import { G5Stack } from '../lib/g5';
 
 const app = new cdk.App();
 
-const NICE_DCV_DISPLAY_DRIVER_URL = 'https://d1uj6qtbmh3dt5.cloudfront.net/nice-dcv-virtual-display-x64-Release.msi';
-//const NICE_DCV_SERVER_URL = 'https://d1uj6qtbmh3dt5.cloudfront.net/nice-dcv-server-x64-Release.msi'; need to dive deeper into why the latest version stops working.
-const NICE_DCV_SERVER_URL = 'https://d1uj6qtbmh3dt5.cloudfront.net/2023.0/Servers/nice-dcv-server-x64-Release-2023.0-15487.msi';              
-const GRID_SW_CERT_URL = 'https://nvidia-gaming.s3.amazonaws.com/GridSwCert-Archive/GridSwCertWindows_2021_10_2.cert';
-const CHROME_URL = 'https://dl.google.com/tag/s/appname=Google%20Chrome&needsadmin=true&ap=x64-stable-statsdef_0&brand=GCEA/dl/chrome/install/googlechromestandaloneenterprise64.msi';
+const NICE_DCV_DISPLAY_DRIVER_URL =
+  'https://d1uj6qtbmh3dt5.cloudfront.net/nice-dcv-virtual-display-x64-Release.msi';
+const NICE_DCV_SERVER_URL =
+  'https://d1uj6qtbmh3dt5.cloudfront.net/nice-dcv-server-x64-Release.msi';
+const GRID_SW_CERT_URL =
+  'https://nvidia-gaming.s3.amazonaws.com/GridSwCert-Archive/GridSwCertWindows_2021_10_2.cert';
+const CHROME_URL =
+  'https://dl.google.com/tag/s/appname=Google%20Chrome&needsadmin=true&ap=x64-stable-statsdef_0&brand=GCEA/dl/chrome/install/googlechromestandaloneenterprise64.msi';
 const SEVEN_ZIP_URL = 'https://www.7-zip.org/a/7z2201-x64.msi';
 
-const EC2_KEYPAIR_NAME = 'CHANGE_ME';
+const EC2_KEYPAIR_NAME = 'egilkh';
 const VOLUME_SIZE_GIB = 200;
-const OPEN_PORTS = [8443];
+// Windows RDP Port
+const OPEN_PORTS = [3389];
 const ALLOW_INBOUND_CIDR = '0.0.0.0/0';
-const ACCOUNT_ID = 'CHANGE_ME';
-const REGION = 'eu-west-2';
+
+const ACCOUNT_ID = '318075462624';
+const REGION = 'eu-west-1';
 
 new G4DNStack(app, 'CloudGamingsOnG4DN', {
   niceDCVDisplayDriverUrl: NICE_DCV_DISPLAY_DRIVER_URL,
@@ -27,7 +32,7 @@ new G4DNStack(app, 'CloudGamingsOnG4DN', {
   sevenZipUrl: SEVEN_ZIP_URL,
   chromeUrl: CHROME_URL,
   gridSwCertUrl: GRID_SW_CERT_URL,
-  instanceSize: ec2.InstanceSize.XLARGE,
+  instanceSize: ec2.InstanceSize.XLARGE2,
   ec2KeyName: EC2_KEYPAIR_NAME,
   volumeSizeGiB: VOLUME_SIZE_GIB,
   openPorts: OPEN_PORTS,
@@ -48,7 +53,7 @@ new G5Stack(app, 'CloudGamingOnG5', {
   sevenZipUrl: SEVEN_ZIP_URL,
   chromeUrl: CHROME_URL,
   gridSwCertUrl: GRID_SW_CERT_URL,
-  instanceSize: ec2.InstanceSize.XLARGE,
+  instanceSize: ec2.InstanceSize.XLARGE2,
   ec2KeyName: EC2_KEYPAIR_NAME,
   volumeSizeGiB: VOLUME_SIZE_GIB,
   openPorts: OPEN_PORTS,
@@ -69,7 +74,7 @@ new G4ADStack(app, 'CloudGamingsOnG4AD', {
   chromeUrl: CHROME_URL,
   sevenZipUrl: SEVEN_ZIP_URL,
   gridSwCertUrl: GRID_SW_CERT_URL,
-  instanceSize: ec2.InstanceSize.XLARGE,
+  instanceSize: ec2.InstanceSize.XLARGE2,
   ec2KeyName: EC2_KEYPAIR_NAME,
   volumeSizeGiB: VOLUME_SIZE_GIB,
   openPorts: OPEN_PORTS,
